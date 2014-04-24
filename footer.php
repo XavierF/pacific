25e41a4eb3d064f61dcedea67fd56bd317bb6264
XavierF <xavierf424@gmail.com>
2014-04-23 22:49:01 -0700

    footer

--- footer.php
+++ footer.php
@@ -3,17 +3,13 @@
 				<div class="container">
 
 				<div id="inner-footer" class="wrap clearfix">
-	<ul class="social">
-		<li><a href="https://www.facebook.com/pages/Yoga-Treat/1480466952175295" target="_blank"><i class="icon facebook"></i></a></li>
-		<li><a href="http://instagram.com/yogatreat?" target="_blank"><i class="icon instagram"></i></a></li>
-		<li><a href="http://twitter.com" target="_blank"><i class="icon twitter"></i></a></li>
-	</ul>
 
 					<nav role="navigation">
-							<?php bones_footer_links(); ?>
+						<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					  <?php bones_footer_links(); ?>
 					</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>.</p>
+					
 
 				</div> <!-- end #inner-footer -->
 

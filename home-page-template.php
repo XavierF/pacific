8bf610b919e26a5141b3b589f2ab87a43a9aac9d
XavierF <xavierf424@gmail.com>
2014-05-01 17:38:03 -0700

    home page template and sum css edits

--- home-page-template.php
+++ home-page-template.php
@@ -124,25 +124,31 @@
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_1_url'); ?>" class="bike">
 					<div class="thumb" style="background: url('<?php the_field('page_1_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
-					<p><?php the_field('page_1_text'); ?></p>
-					<p><?php the_field('page_1_price'); ?></p>
-					<p><?php the_field('page_1_available'); ?></p>
+					<div class="thumb-info">
+						<p><?php the_field('page_1_text'); ?></p>
+						<p><?php the_field('page_1_price'); ?></p>
+						<p><?php the_field('page_1_available'); ?></p>
+					</div>
 				</a>
 			</div><!-- end .col-sm-4 -->
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_2_url'); ?>" class="bike">
 					<div class="thumb" style="background: url('<?php the_field('page_2_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
-					<p><?php the_field('page_2_text'); ?></p>
-					<p><?php the_field('page_1_price'); ?></p>
-					<p><?php the_field('page_1_available'); ?></p>
+					<div class="thumb-info">
+						<p><?php the_field('page_2_text'); ?></p>
+						<p><?php the_field('page_2_price'); ?></p>
+						<p><?php the_field('page_2_available'); ?></p>
+					</div>
 				</a>
 			</div><!-- end .col-sm-4 -->
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_3_url'); ?>" class="bike">
 					<div class="thumb" style="background: url('<?php the_field('page_3_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
-					<p><?php the_field('page_3_text'); ?></p>
-					<p><?php the_field('page_1_price'); ?></p>
-					<p><?php the_field('page_1_available'); ?></p>
+					<div class="thumb-info">
+						<p><?php the_field('page_3_text'); ?></p>
+						<p><?php the_field('page_3_price'); ?></p>
+						<p><?php the_field('page_3_available'); ?></p>
+					</div>
 				</a>
 			</div><!-- end .col-sm-4-->
 		</div><!-- end .row-->
@@ -150,25 +156,31 @@
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_4_url'); ?>" class="bike">
 					<div class="thumb" style="background: url('<?php the_field('page_4_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
-					<p><?php the_field('page_4_text'); ?></p>
-					<p><?php the_field('page_1_price'); ?></p>
-					<p><?php the_field('page_1_available'); ?></p>
+					<div class="thumb-info">
+						<p><?php the_field('page_4_text'); ?></p>
+						<p><?php the_field('page_4_price'); ?></p>
+						<p><?php the_field('page_4_available'); ?></p>
+					</div>
 				</a>
 			</div><!-- end .col-sm-4 -->
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_5_url'); ?>" class="bike">
 					<div class="thumb" style="background: url('<?php the_field('page_5_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
-					<p><?php the_field('page_5_text'); ?></p>
-					<p><?php the_field('page_1_price'); ?></p>
-					<p><?php the_field('page_1_available'); ?></p>
+					<div class="thumb-info">
+						<p><?php the_field('page_5_text'); ?></p>
+						<p><?php the_field('page_5_price'); ?></p>
+						<p><?php the_field('page_5_available'); ?></p>
+					</div>
 				</a>
 			</div><!-- end .col-sm-4 -->
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_6_url'); ?>" class="bike">
 					<div class="thumb" style="background: url('<?php the_field('page_6_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
-					<p><?php the_field('page_6_text'); ?></p>
-					<p><?php the_field('page_1_price'); ?></p>
-					<p><?php the_field('page_1_available'); ?></p>
+					<div class="thumb-info">
+						<p><?php the_field('page_6_text'); ?></p>
+						<p><?php the_field('page_6_price'); ?></p>
+						<p><?php the_field('page_6_available'); ?></p>
+					</div>
 				</a>
 			</div><!-- end .col-sm-4 -->
 		</div><!-- end .row -->

cf41b91ff96993499b891e1eaf34d59b9d747abb
XavierF <xavierf424@gmail.com>
2014-05-07 20:39:49 -0700

    home page and product page update

--- home-page-template.php
+++ home-page-template.php
@@ -20,6 +20,7 @@
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<div class="container">
 		<div class="carousel-inner">
 
 			<div class="item active" style="background: url('<?php the_field('slide_1_bg');?>') 50% 50% no-repeat; background-size: cover;">
@@ -82,41 +83,12 @@
 			</div> <!-- end .container-->
 		</div><!-- end .item -->
 
-		<div class="item" style="background: url('<?php the_field('slide_5_bg');?>') 50% 50% no-repeat; background-size: cover;">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<blockquote>
-							<?php the_field('slide_5_caption'); ?>
-							<small>
-								<?php the_field('slide_5_name'); ?>
-							</small>
-						</blockquote>
-					</div><!-- end .col-lg-12-->
-				</div><!-- end .row-->
-			</div> <!-- end .container-->
-		</div><!-- end .item -->
-
-		<div class="item" style="background: url('<?php the_field('slide_6_bg');?>') 50% 50% no-repeat; background-size: cover;">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-12">
-						<blockquote>
-							<?php the_field('slide_6_caption'); ?>
-							<small>
-								<?php the_field('slide_6_name'); ?>
-							</small>
-						</blockquote>
-					</div>
-				</div><!-- end .row-->
-			</div> <!-- end .container-->
-		</div><!-- end .item -->
-
 	</div><!-- end .carousel-inner -->
 	<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
 	<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
 </div><!-- end of carousel -->
 </div> <!-- end #banner-->
+</div>
 <!-- page thumbnails start here -->
 <section id="items" role="promo" class="thumb-area">
 	<div class="container">

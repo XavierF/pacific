0f2b75e616464d1a8be6039093f1910bae9ce834
XavierF <xavierf424@gmail.com>
2014-05-08 15:42:38 -0700

    home page template and css

--- home-page-template.php
+++ home-page-template.php
@@ -11,7 +11,7 @@
 <section id="description" role="tagline">
 <div class="container">
 		<div class="row">
-			<div class="col-sm-12 clearfix">
+			<div class="col-sm-12">
 					<h1 class="tag"><?php bloginfo('description'); ?></h1>
 			</div><!-- "col-sm-12" -->
 		</div><!-- row -->
@@ -19,12 +19,12 @@
 </section>
 
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<div class="container">
 	<div id="myCarousel" class="carousel slide" data-ride="carousel">
-		<div class="container">
 		<div class="carousel-inner">
 
-			<div class="item active" style="background: url('<?php the_field('slide_1_bg');?>') 50% 50% no-repeat; background-size: cover;">
-				<div class="container">
+			<div class="item active" style="background: url('<?php the_field('slide_1_bg');?>') 45% center no-repeat; background-size: cover;">
+				
 					<div class="row">
 						<div class="col-lg-12">
 							<blockquote>
@@ -35,11 +35,11 @@
 							</blockquote>
 						</div><!-- end .col-lg-12-->
 					</div><!-- end .row-->
-				</div> <!-- end .container-->
+			
 		  </div><!-- end .item active -->
 
-		<div class="item" style="background: url('<?php the_field('slide_2_bg');?>') 50% 50% no-repeat; background-size: cover;">
-			<div class="container">
+		<div class="item" style="background: url('<?php the_field('slide_2_bg');?>') 45% center no-repeat; background-size: cover;">
+			
 				<div class="row">
 					<div class="col-lg-12">
 						<blockquote>
@@ -50,11 +50,10 @@
 						</blockquote>
 					</div><!-- end .col-lg-12-->
 				</div><!-- end .row-->
-			</div> <!-- end .container-->
 		</div><!-- end .item -->
 
-		<div class="item" style="background: url('<?php the_field('slide_3_bg');?>') 50% 50% no-repeat; background-size: cover;">
-			<div class="container">
+		<div class="item" style="background: url('<?php the_field('slide_3_bg');?>') 45% center no-repeat; background-size: cover;">
+			
 				<div class="row">
 					<div class="col-lg-12">
 						<blockquote>
@@ -65,11 +64,10 @@
 						</blockquote>
 					</div><!-- end .col-lg-12-->
 				</div><!-- end .row-->
-			</div> <!-- end .container-->
 		</div><!-- end .item -->
 
-		<div class="item" style="background: url('<?php the_field('slide_4_bg');?>') 50% 50% no-repeat; background-size: cover;">
-			<div class="container">
+		<div class="item" style="background: url('<?php the_field('slide_4_bg');?>') 45% center no-repeat; background-size: cover;">
+			
 				<div class="row">
 					<div class="col-lg-12">
 						<blockquote>
@@ -80,22 +78,19 @@
 						</blockquote>
 					</div><!-- end .col-lg-12-->
 				</div><!-- end .row-->
-			</div> <!-- end .container-->
 		</div><!-- end .item -->
-
 	</div><!-- end .carousel-inner -->
 	<a class="left carousel-control" href="#myCarousel" data-slide="prev">‹</a>
 	<a class="right carousel-control" href="#myCarousel" data-slide="next">›</a>
 </div><!-- end of carousel -->
-</div> <!-- end #banner-->
-</div>
+</div><!-- end .container -->
 <!-- page thumbnails start here -->
-<section id="items" role="promo" class="thumb-area">
+<section id="items" role="promo" class="thumbs">
 	<div class="container">
 		<div class="row">
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_1_url'); ?>" class="bike">
-					<div class="thumb" style="background: url('<?php the_field('page_1_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
+					<div class="thumb" style="background: url('<?php the_field('page_1_img');?>') center center no-repeat; background-size: cover; width:360px; height:240px;"></div>
 					<div class="thumb-info">
 						<p><?php the_field('page_1_text'); ?></p>
 						<p><?php the_field('page_1_price'); ?></p>
@@ -105,7 +100,7 @@
 			</div><!-- end .col-sm-4 -->
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_2_url'); ?>" class="bike">
-					<div class="thumb" style="background: url('<?php the_field('page_2_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
+					<div class="thumb" style="background: url('<?php the_field('page_2_img');?>') center center no-repeat; background-size: cover; width:360px; height:240px;"></div>
 					<div class="thumb-info">
 						<p><?php the_field('page_2_text'); ?></p>
 						<p><?php the_field('page_2_price'); ?></p>
@@ -115,7 +110,7 @@
 			</div><!-- end .col-sm-4 -->
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_3_url'); ?>" class="bike">
-					<div class="thumb" style="background: url('<?php the_field('page_3_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
+					<div class="thumb" style="background: url('<?php the_field('page_3_img');?>') center center no-repeat; background-size: cover; width:360px; height:240px;"></div>
 					<div class="thumb-info">
 						<p><?php the_field('page_3_text'); ?></p>
 						<p><?php the_field('page_3_price'); ?></p>
@@ -127,7 +122,7 @@
 		<div class="row">
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_4_url'); ?>" class="bike">
-					<div class="thumb" style="background: url('<?php the_field('page_4_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
+					<div class="thumb" style="background: url('<?php the_field('page_4_img');?>') center center no-repeat; background-size: cover; width:360px; height:240px;"></div>
 					<div class="thumb-info">
 						<p><?php the_field('page_4_text'); ?></p>
 						<p><?php the_field('page_4_price'); ?></p>
@@ -137,7 +132,7 @@
 			</div><!-- end .col-sm-4 -->
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_5_url'); ?>" class="bike">
-					<div class="thumb" style="background: url('<?php the_field('page_5_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
+					<div class="thumb" style="background: url('<?php the_field('page_5_img');?>') center center no-repeat; background-size: cover; width:360px; height:240px;"></div>
 					<div class="thumb-info">
 						<p><?php the_field('page_5_text'); ?></p>
 						<p><?php the_field('page_5_price'); ?></p>
@@ -147,7 +142,7 @@
 			</div><!-- end .col-sm-4 -->
 			<div class="col-sm-4">
 				<a href="<?php the_field('page_6_url'); ?>" class="bike">
-					<div class="thumb" style="background: url('<?php the_field('page_6_img');?>') center center no-repeat; background-size: cover; width:290px; height:193px;"></div>
+					<div class="thumb" style="background: url('<?php the_field('page_6_img');?>') center center no-repeat; background-size: cover; width:360px; height:240px;"></div>
 					<div class="thumb-info">
 						<p><?php the_field('page_6_text'); ?></p>
 						<p><?php the_field('page_6_price'); ?></p>
@@ -158,6 +153,7 @@
 		</div><!-- end .row -->
 	</div><!-- end .container -->
 </section><!-- end page thumbs -->
+
 
 <?php endwhile; endif; ?>
 

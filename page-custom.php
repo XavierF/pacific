06d23f42f577cad9ea8813412c30e05b6a44be4c
XavierF <xavierf424@gmail.com>
2014-05-10 17:19:45 -0700

    custom and contact pages html

--- page-custom.php
+++ page-custom.php
@@ -3,72 +3,66 @@
 Template Name: Custom Page Template
 */
 ?>
-
 <?php get_header(); ?>
 
 <div class="container">
+	<div id="page-content">
+			<header class="custom-page-header">
+				<h1 class="page-title"><?php the_title(); ?></h1>
+			</header> <!-- end page header -->
 
-			<div id="page-content">
-				<header class="custom-page-header">
-						<h1 class="page-title"><?php the_title(); ?></h1>
-				</header> <!-- end page header -->
+			<div id="inner-content" class="wrap clearfix">
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
 
-				<div id="inner-content" class="wrap clearfix">
-
+					<section class="entry-content clearfix" itemprop="articleBody">
 						
+						<div class="row">
+							<div class="sub-header col-md-12">
+								<h1><?php the_field('page_tagline'); ?></h1>
+							</div>
+						</div>
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+						<div class="row">
+							<div class="col-md-6">
+								<?php the_content(); ?>
+							</div><!-- .col-md-6 -->
+							<div class="col-md-6"><!-- right column -->
+								<?php the_field('right_column'); ?>
+								<hr>
+								<h4>"Best prices and service in town"</h4>
+								<a class="yelp" href="http://www.yelp.com/biz/pacific-e-bike-berkeley" title="yelp" target="_blank"></a>
+							</div><!-- .col-md-6 -->
+						</div><!-- .row -->
+					</section> <!-- end article section -->
 
-							<article id="post-<?php the_ID(); ?>" <?php post_class( 'clearfix' ); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
+					<footer class="article-footer">
+						<p class="clearfix"><?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ' ', '' ); ?></p>
+					</footer> <!-- end article footer -->
 
-								
+				</article> <!-- end article -->
 
-								<section class="entry-content clearfix" itemprop="articleBody">
-									<div class="row">
-										<div class="col-md-6">
-											<?php the_content(); ?>
-										</div><!-- .col-md-5 -->
-										<div class="col-md-6">
-											<?php the_field('right_column'); ?>
+					<?php endwhile; else : ?>
 
-
-<hr>
-<h4>"Best prices and service in town"</h4>
-<a class="yelp" href="http://www.yelp.com/biz/pacific-e-bike-berkeley" title="yelp" target="_blank"></a>
-										</div><!-- .col-md-5 -->
-									</div><!-- .row -->
-								</section> <!-- end article section -->
-
-								<footer class="article-footer">
-									<p class="clearfix"><?php the_tags( '<span class="tags">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ' ', '' ); ?></p>
-
-								</footer> <!-- end article footer -->
-
-							</article> <!-- end article -->
-
-							<?php endwhile; else : ?>
-
-									<article id="post-not-found" class="hentry clearfix">
-											<header class="article-header">
-												<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
-										</header>
-											<section class="entry-content">
-												<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
-										</section>
-										<footer class="article-footer">
-												<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
-										</footer>
-									</article>
+				<article id="post-not-found" class="hentry clearfix">
+					<header class="article-header">
+							<h1><?php _e( 'Oops, Post Not Found!', 'bonestheme' ); ?></h1>
+					</header>
+						<section class="entry-content">
+							<p><?php _e( 'Uh Oh. Something is missing. Try double checking things.', 'bonestheme' ); ?></p>
+					</section>
+					<footer class="article-footer">
+							<p><?php _e( 'This is the error message in the page-custom.php template.', 'bonestheme' ); ?></p>
+					</footer>
+				</article>
 
 							<?php endif; ?>
 
 						
+			</div> <!-- end #inner-content -->
 
-						
-				</div> <!-- end #inner-content -->
+	</div> <!-- end #content -->
 
-			</div> <!-- end #content -->
-
-			</div> <!-- end .container -->
+</div> <!-- end .container -->
 
 <?php get_footer(); ?>

0745621334c5f3283b61f0324f4035236ceb66b2
XavierF <xavierf424@gmail.com>
2014-05-13 16:53:45 -0700

    html edit

--- page-custom.php
+++ page-custom.php
@@ -39,7 +39,6 @@ Template Name: Custom Page Template
 
 									<div class="visible-xs visible-sm"><!-- google map starts here -->
 										<h2>Find us in Berkeley</h2>
-										<h2>Find us in Berkeley</h2>
 										<p>(510) 843 - 2453</p>
 										<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3150.1380694641416!2d-122.29379259999999!3d37.8570597!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80857ef2200c0469%3A0xe800db178ed7e187!2s843+Carleton+St!5e0!3m2!1sen!2sus!4v1399783363925" width="100%" height="300" frameborder="0" style="border:0"></iframe>
 									</div><!-- google map ends here -->
